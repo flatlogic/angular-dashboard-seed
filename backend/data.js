2a8f04ad56537e78bf57b39a3633a5581b22b09c
smartapant <smartapant@gmail.com>
2015-02-02 17:31:41 +0300

    Folders renamed.

--- backend/data.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by smartapant on 27.1.15.
- */
-module.exports = function(moduleName) {
-    var provider = 'stubs';
-    return require('./data/' + provider + '/' + moduleName);
-};
