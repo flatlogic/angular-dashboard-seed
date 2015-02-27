52b2dba6e16aa87c62e2b7c8ac9b70480f0485f3
smartapant <smartapant@gmail.com>
2015-02-27 17:27:49 +0300

    OnBlur validation.

--- src/app/modules/common/validate/validation.js
+++ src/app/modules/common/validate/validation.js
@@ -2,28 +2,47 @@
   'use strict';
   var common = angular.module('app.common');
 
-  common.directive('validatableSubmit', validatableSubmit);
+  common.directive('validatableSubmit', validatableSubmit)
+        .directive('showErrors', showErrors);
 
   validatableSubmit.$inject = ['$parse'];
   function validatableSubmit($parse) {
       return {
         restrict: 'A',
         require: 'form',
-        link: function(scope, form, attrs) {
+        link: function(scope, $form, attrs, formCtrl) {
           var submit = $parse(attrs.validatableSubmit);
-          var formScope = scope[attrs.name];
-          form.submit(function(evt) {
-            formScope.submitted = true;
-            !scope.$$phase && scope.$apply();
-            if (!formScope.$valid) {
-              return false;
-            }
-            scope.$apply(function() {
+          $form.submit(function(evt) {
+            scope.$broadcast('show-errors-check-validity');
+            formCtrl.$valid && scope.$apply(function() {
               submit(scope, {$event: evt});
             });
           });
         }
       };
   };
+
+  function showErrors() {
+    return {
+      restrict: 'A',
+      require: '^form',
+      link: function(scope, $el, attrs, formCtrl) {
+        var $input = $el.find('[name]');
+        var inputName = $input.attr('name');
+        $input.bind('blur', function() {
+          console.log(formCtrl[inputName]);
+          $el.toggleClass('has-error', formCtrl[inputName].$invalid);
+        });
+        scope.$on('show-errors-check-validity', function() {
+          $el.toggleClass('has-error', formCtrl[inputName].$invalid);
+        });
+        scope.$on('show-errors-reset', function() {
+          $timeout(function() {
+            $el.removeClass('has-error');
+          }, 0, false);
+        });
+      }
+    }
+  }
 
 })();
