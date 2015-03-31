33675d245dfb6ae16a592d0e30907e5606f0e29a
philip.daineka@gmail.com <philip.daineka@gmail.com>
2015-03-31 15:54:46 +0300

    enhance dashboard top widget look & feel. close #6

--- src/app/modules/dashboard/dashboard.js
+++ src/app/modules/dashboard/dashboard.js
@@ -1,28 +1,30 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular.module('app.dashboard')
-        .controller('dashboardController', dashboardController);
+  angular.module('app.dashboard')
+    .controller('dashboardController', dashboardController);
 
-    dashboardController.$inject = ['$scope','posts', '$sce'];
-    function dashboardController($scope, posts, $sce) {
-        $scope.posts = posts;
-        $scope.alerts = [
-            { type: 'warning', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Warning:</span> Best check yo self, you\'re not looking too good.') },
-            { type: 'success', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Success:</span> You successfully read this important alert message.') },
-            { type: 'info', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Info:</span> This alert needs your attention, but it\'s not super important.') },
-            { type: 'danger', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Danger:</span> Change this and that and try again.'
-                                + '<a class="btn btn-default btn-xs pull-right mr" href="#">Ignore</a>'
-                                + '<a class="btn btn-danger btn-xs pull-right mr-xs" href="#">Take this action</a>') }
-        ];
+  dashboardController.$inject = ['$scope', '$sce', 'posts', 'postsUtils'];
+  function dashboardController($scope, $sce, posts, postsUtils) {
+    $scope.posts = posts;
+    $scope.postsLastMonth = postsUtils.postsDuringInterval(posts, 30);
+    $scope.lastEditedPost = postsUtils.lastEdited(posts);
+    $scope.alerts = [
+      { type: 'warning', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Warning:</span> Best check yo self, you\'re not looking too good.') },
+      { type: 'success', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Success:</span> You successfully read this important alert message.') },
+      { type: 'info', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Info:</span> This alert needs your attention, but it\'s not super important.') },
+      { type: 'danger', msg: $sce.trustAsHtml('<span class="fw-semi-bold">Danger:</span> Change this and that and try again.'
+      + '<a class="btn btn-default btn-xs pull-right mr" href="#">Ignore</a>'
+      + '<a class="btn btn-danger btn-xs pull-right mr-xs" href="#">Take this action</a>') }
+    ];
 
-        $scope.addAlert = function() {
-            $scope.alerts.push({type: 'warning', msg: $sce.trustAsHtml('Another alert!')});
-        };
+    $scope.addAlert = function() {
+      $scope.alerts.push({type: 'warning', msg: $sce.trustAsHtml('Another alert!')});
+    };
 
-        $scope.closeAlert = function(index) {
-            $scope.alerts.splice(index, 1);
-        };
-    }
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
+    };
+  }
 
 })();
--- src/app/modules/dashboard/top_widgets/widget.js
+++ src/app/modules/dashboard/top_widgets/widget.js
@@ -2,46 +2,18 @@
   'use strict';
 
   angular.module('app.dashboard')
-    .directive('dashTopWidget', dashTopWidget);
+    .directive('widgetStat', widgetStat);
 
-  dashTopWidget.$inject = ['postsUtils'];
-  function dashTopWidget(postsUtils) {
-
-    function list($scope) {
-      if ($scope.interval) {
-        $scope.postsNumber = $scope.descriptionTop = postsUtils.postsDuringInterval($scope.allPosts, $scope.interval).length;
-      } else {
-        $scope.postsNumber = $scope.descriptionTop = $scope.allPosts.length;
-      }
-    }
-
-    function lastEdited($scope) {
-        $scope.postsNumber = 1;
-        $scope.lastEdited = postsUtils.lastEdited($scope.allPosts);
-    }
-
-    var types = {
-      lastEdited: lastEdited,
-      list: list
-    };
-
+  function widgetStat() {
     return {
       restrict: 'EA',
+      transclude: true,
       scope: {
         'widgetClass': '@',
         'iconClass': '@',
-        'descriptionTop': '@',
-        'descriptionBottom': '@',
-        'widgetSref': '@',
-        'type': '@',
-        'interval': '@',
-        'linkMsg': '@',
-        'allPosts': '=posts'
+        'iconText': '@'
       },
-      controller: ['$scope', '$state', function($scope, $state) {
-        types[$scope.type || 'list']($scope);
-      }],
-      templateUrl: 'app/modules/dashboard/top_widgets/dash_top_widget.html'
+      templateUrl: 'app/modules/dashboard/top_widgets/widget_stat.html'
     }
   }
 
